8b161dbd3a71d4e482cc2652c27fa8231fe878c2
orzofim9 <orzofim9@gmail.com>
2019-03-17 11:52:52 +0200

    progress one

--- src/App.js
+++ src/App.js
@@ -1,59 +1,86 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CroppedPhoto from './CroppedPhoto'
 import shuffle from 'shuffle-array';
+import PuzzleBord from './PuzzleBord';
+import Bord from './Bord';
+import GridLayout from 'react-grid-layout';
+/*import {Button} from 'react-bootstrap';*/
+import Button from 'react-bootstrap/Button';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             input_num: 0
+            
         }
     }
 
     handleInputChange(e) {
-        console.log(e.target.value);
-        this.setState({input_num: e.target.value})
+        //console.log("button"+ e);
+        this.setState({input_num: e});
+        var x = document.getElementById("500");
+        console.log("image is ======== "+x) 
+        x.style.display = "block";
+        var y = document.getElementById("400");
+        y.style.display = "none";
     }
-
+    
   render() {
       let arr = [];
       const gridSize = this.state.input_num;
       const percentage = 100 / (gridSize - 1);
-
+      let boxMap= [];
+      let str=[];
+   
       for (let i=0; i<gridSize * gridSize; i++) {
+        
+       
+       /* let boxOrder ={
+          BOrder: i+100,
+          };*/
+
           let current_tile = {
               xpos: (percentage * (i % gridSize)) + '%',
               ypos: (percentage * Math.floor(i / gridSize)) + '%',
-          };
+              numName: i
+            };
+          console.log("XPOS=" + current_tile.xpos);
+          console.log("YPOS=" + current_tile.ypos);
+          console.log(" numName=" + current_tile.numName);
+         // console.log(" boxName=" + boxOrder);
           arr.push(current_tile)
+         // boxMap.push(boxOrder)
       }
+      
       shuffle(arr);
+
 
     return (
       <div className="App">
-        {/*<header className="App-header">*/}
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          {/*<p>*/}
-            {/*Edit <code>src/App.js</code> and save to reload.*/}
-          {/*</p>*/}
-            {/*/!*<CroppedPhoto img_path="./image.jpg" startpx="100"/>*!/*/}
-
-          {/*<a*/}
-            {/*className="App-link"*/}
-            {/*href="https://reactjs.org"*/}
-            {/*target="_blank"*/}
-            {/*rel="noopener noreferrer"*/}
-          {/*>*/}
-            {/*Learn React*/}
-          {/*</a>*/}
-        {/*</header>*/}
-          <img src="./image.jpg" width="1100" height="1100"/>
-          <div>
-          <input type="text" name="fname" onChange={e => this.handleInputChange(e)}/>
-          {arr.map(tile => <CroppedPhoto img_path="./image.jpg" startpxX={tile.xpos} startpxY={tile.ypos} gridSize={gridSize}/>)}
+         
+          {/*<input type="text" name="fname" onChange={e => this.handleInputChange(e)}/>*/}
+         <div className="buttons" id= "400">
+          <Button variant="success" size='lg' onClick={e => this.handleInputChange(e=3)} >Easy</Button>
+          <Button variant= "success" size='lg' onClick={e => this.handleInputChange(e=4)}>Medium</Button>
+          <Button variant="success" size='lg' onClick={e => this.handleInputChange(e=5)}>Hard</Button>
+          <Button variant="success" size='lg' onClick={e => this.handleInputChange(e=6)}>Professional</Button>
+         </div>
+     
+        <img src="./image.jpg" className="im" id="500" width="250" height="320"   /*draggable = "true"*/ />   
+          { /*boxMap.map(bo =>   */<Bord gridSize={gridSize} /*boxMap={boxMap} boxOrder={bo.BOrder} />)}*//>}
+          
+           {/*  <div className="gamePieces" >
+               {boxMap.map(bo =>   <PuzzleBord gridSize={gridSize} boxOrder={bo.BOrder} newRow={bo.newRow} />)}
+    </div>*/}  
+     
+       
+          <div className= "pieces" style= {{paddingLeft: '0px', display : 'grid', 'grid-template-columns': 'repeat('+(gridSize*3)+',auto', width : '0px'}}>
+         {arr.map(tile => <CroppedPhoto img_path="./image.jpg" startpxX={tile.xpos} startpxY={tile.ypos} gridSize={gridSize} idNum={tile.numName}/>)}
           </div>
+        
       </div>
     );
   }
